f17948f71e694ebde7af567e4f923d8b7dc30bc9
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-11-20 15:14:19 -0200

    add bucket supervisor

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -16,10 +16,20 @@ defmodule KV.RegistryTest do
     assert KV.Bucket.get(bucket, "milk") == 1
   end
 
-  test "removes buckets on exit", %{registry: registry} do
+  test "removes bucket on exit", %{registry: registry} do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
     Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    Agent.stop(bucket, :shutdown)
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
