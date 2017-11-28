92c496b2b6b42f2718a7e64e096a727cc3925827
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-11-28 17:52:36 -0200

    add task supervisor

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -6,8 +6,11 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || raise "missing $PORT environment variable")
+
     children = [
-      {Task, fn -> KVServer.accept(4040) end}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
